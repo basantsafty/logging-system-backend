eb27967465f3d95b82ba28f17b26b78bbd8f305d
Bassant <basantsafty@gamil.com>
2026-05-24 04:34:12 +0300

    register-fix-adding validation and API genaration

--- src/developer/developer.controler.js
+++ src/developer/developer.controler.js
@@ -1,9 +1,23 @@
 import Developer from "./developer.model.js"
 import { request, response } from "express"
-const apiKey=1234567 // This is a placeholder API key.
+import jwt from "jsonwebtoken"
+import crypto from "crypto"
+
 export const registerUser = async(request,response,next)=>{
     try {
         const {name,email,password} = request.body
+        if(!name || !email || !password){
+            return response.status(400).json({
+                message:"Name, email, and password are required"
+            })
+        }
+        const existingUser = await Developer.findOne({email})
+        if(existingUser){
+            return response.status(409).json({
+                message:"Email already exists"
+            })
+        }   
+        const apiKey = crypto.randomBytes(16).toString("hex"); //creating random api key for each user
         const user = await Developer.create({name,email,password,apiKey})
         response.status(201).json({
             message:"User created successfully",
